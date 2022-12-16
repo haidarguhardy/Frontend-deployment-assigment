962fdb1834f9e36f2715de4351402cc1cf266241
haidarguhardy <haidar.19005@mhs.unesa.ac.id>
2022-12-16 11:27:19 +0700

    add: add url to task2

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = 'YOURDEPLOYURL'; // TODO: replace this
+const url = 'https://haidar-frontend-deployment.netlify.app/'; // TODO: replace this
 const studentName = 'Haidar Guhardy Muhammad'; // TODO: replace this
 const studentId = 'FE2653199'; // TODO: replace this
 
